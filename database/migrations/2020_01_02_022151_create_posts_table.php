7dcc9331f7e6c6ea437a17623e17715e4bb7f0c9
Rannie Ollit <rannie.ollit@petnet.com.ph>
2020-01-02 15:34:08 +0800

    Added the migration comment

--- database/migrations/2020_01_02_022151_create_posts_table.php
+++ database/migrations/2020_01_02_022151_create_posts_table.php
@@ -15,6 +15,9 @@ class CreatePostsTable extends Migration
     {
         // After we added the another mysql connection settings, call the connection function from
         // the Schema Class to specify the database connection path of the table
+        //
+        // Also added some logic in migration, since it didn't created the migrations table
+        // in able to check if the migration exists.
         if (!Schema::connection('mysql2')->hasTable('posts')) {
             Schema::connection('mysql2')->create('posts', function (Blueprint $table) {
                 $table->bigIncrements('id');
