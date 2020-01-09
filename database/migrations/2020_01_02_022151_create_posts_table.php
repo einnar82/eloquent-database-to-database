a4feb88e628a4b11f74bc8bd04619d29f6c30df6
Rannie Ollit <einnar82@gmail.com>
2020-01-09 15:09:58 +0800

    Update 2020_01_02_022151_create_posts_table.php

--- database/migrations/2020_01_02_022151_create_posts_table.php
+++ database/migrations/2020_01_02_022151_create_posts_table.php
@@ -1,5 +1,4 @@
 <?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -7,27 +6,43 @@ use Illuminate\Support\Facades\Schema;
 class CreatePostsTable extends Migration
 {
     /**
+     * The database schema.
+     *
+     * @var \Illuminate\Database\Schema\Builder
+     */
+    protected $schema;
+    /**
+     * Create a new migration instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->schema = Schema::connection($this->getConnection());
+    }
+    /**
+     * Get the migration connection name.
+     *
+     * @return string|null
+     */
+    public function getConnection()
+    {
+        return 'mysql2';
+    }
+    /**
      * Run the migrations.
      *
      * @return void
      */
     public function up()
     {
-        // After we added the another mysql connection settings, call the connection function from
-        // the Schema Class to specify the database connection path of the table
-        //
-        // Also added some logic in migration, since it didn't created the migrations table
-        // in able to check if the migration exists.
-        if (!Schema::connection('mysql2')->hasTable('posts')) {
-            Schema::connection('mysql2')->create('posts', function (Blueprint $table) {
+            $this->schema->create('posts', function (Blueprint $table) {
                 $table->bigIncrements('id');
                 $table->integer('user_id')->default(1);
                 $table->longText('content')->nullable();
                 $table->timestamps();
             });
-        }
     }
-
     /**
      * Reverse the migrations.
      *
@@ -35,8 +50,6 @@ class CreatePostsTable extends Migration
      */
     public function down()
     {
-        if (Schema::connection('mysql2')->hasTable('posts')) {
-            Schema::connection('mysql2')->drop('posts');
-        }
+         $this->schema->dropIfExists('posts');
     }
 }
